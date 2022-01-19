c2613e970993b3c9c8d7ed854a20f3eaa7e7377f
bjornkasin <91956141+bjornkasin@users.noreply.github.com>
2022-01-19 15:23:08 +0100

    Create Rights to PEM file.bat
    Command line to set PEM file rights correct - PEM file need to be open only to current user

---
+++ SSH/Rights to PEM file.bat
@@ -0,0 +1,24 @@
+::# Set Key File Variable:
+::#    Set Key="%UserProfile%\.ssh\id_rsa"
+    Set Key = "C:\TST\HTA+Norway+-+S4+2021+activated+content.pem"
+    
+::# Remove Inheritance:
+    Icacls %Key% /c /t /Inheritance:d
+
+::# Set Ownership to Owner:
+    :: # Key's within %UserProfile%:
+         Icacls %Key% /c /t /Grant %UserName%:F
+
+    :: # Key's outside of %UserProfile%:
+         TakeOwn /F %Key%
+         Icacls %Key% /c /t /Grant:r %UserName%:F
+
+::# Remove All Users, except for Owner:
+    Icacls %Key% /c /t /Remove:g "Authenticated Users" BUILTIN\Administrators BUILTIN Everyone System Users
+
+::# Verify:
+    Icacls %Key%
+
+::# Remove Variable:
+    set "Key="
+  
